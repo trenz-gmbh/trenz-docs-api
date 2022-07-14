083895f00d1a12231909d1410c1a9bb07cbd05d7
Ricardo Boss <mail@ricardoboss.de>
2022-07-14 14:50:00 +0200

    Added one-shot mode (index and immediately exit)

--- Meilidown/Worker.cs
+++ Meilidown/Worker.cs
@@ -7,13 +7,17 @@ namespace Meilidown
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IHostApplicationLifetime _lifetime;
         private readonly IIndexingService _indexingService;
         private readonly ISourcesProvider _sourcesProvider;
         private readonly IFileProcessingService _fileProcessingService;
 
-        public Worker(ILogger<Worker> logger, IIndexingService indexingService, ISourcesProvider sourcesProvider, IFileProcessingService fileProcessingService)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration, IHostApplicationLifetime lifetime, IIndexingService indexingService, ISourcesProvider sourcesProvider, IFileProcessingService fileProcessingService)
         {
             _logger = logger;
+            _configuration = configuration;
+            _lifetime = lifetime;
             _indexingService = indexingService;
             _sourcesProvider = sourcesProvider;
             _fileProcessingService = fileProcessingService;
@@ -29,12 +33,22 @@ namespace Meilidown
                 var indexFiles = await _fileProcessingService.ProcessAsync(files, stoppingToken).ToListAsync(cancellationToken: stoppingToken);
                 await _indexingService.IndexAsync(indexFiles, stoppingToken);
 
+
+                if (_configuration["Meilidown:OneShot"] == "true")
+                {
+                    _logger.LogWarning("OneShot mode enabled, stopping application");
+
+                    break;
+                }
+
 #if DEBUG
                 Console.ReadKey();
 #else
                 await Task.Delay(TimeSpan.FromMinutes(30), stoppingToken);
 #endif
             }
+
+            _lifetime.StopApplication();
         }
 
         private async IAsyncEnumerable<SourceFile> GatherFiles([EnumeratorCancellation] CancellationToken cancellationToken)
