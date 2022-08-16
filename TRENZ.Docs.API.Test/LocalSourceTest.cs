c99ca161a38cd0f8d4b1391f1af1e5e26e43c316
Ricardo Boss <mail@ricardoboss.de>
2022-08-16 15:57:58 +0200

    Test if the two collections are equivalent, ignore order

--- TRENZ.Docs.API.Test/LocalSourceTest.cs
+++ TRENZ.Docs.API.Test/LocalSourceTest.cs
@@ -96,6 +96,6 @@ public class LocalSourceTest
     {
         var files = source.FindFiles(new(pattern)).Select(sf => sf.RelativePath);
 
-        Assert.AreEqual(string.Join(Path.DirectorySeparatorChar, relativePaths), string.Join(Path.DirectorySeparatorChar, files));
+        CollectionAssert.AreEquivalent(relativePaths.ToArray(), files.ToArray());
     }
 }
