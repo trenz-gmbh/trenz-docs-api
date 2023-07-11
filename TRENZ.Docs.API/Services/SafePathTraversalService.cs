cffc6f5c700ae5cfa6ff5fee9fe5682813c91203
Sören Kuklau <chucker23n+github@gmail.com>
2023-07-11 11:18:03 +0200

    #36: add brief explanation

--- TRENZ.Docs.API/Services/SafePathTraversalService.cs
+++ TRENZ.Docs.API/Services/SafePathTraversalService.cs
@@ -6,9 +6,15 @@ using FSPath = System.IO.Path;
 
 namespace TRENZ.Docs.API.Services;
 
+/// <summary>
+/// Combines a root path with a given relative path, avoiding some naïve
+/// attempts to escape the jail (such as passing `/` or `..` in the relative
+/// path).
+/// </summary>
 public class SafeFileSystemPathTraversalService : ISafeFileSystemPathTraversalService
 {
-    private static SecurityException PreventedPathTraversal => new("Prevented an attempt to traverse a path outside the source's root.");
+    private static SecurityException PreventedPathTraversal
+        => new("Prevented an attempt to traverse a path outside the source's root.");
 
     public string Traverse(string root, string path)
     {
