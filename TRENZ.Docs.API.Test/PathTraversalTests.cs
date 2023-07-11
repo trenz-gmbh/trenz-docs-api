b2c2a721fd736730975b2d68dd9e6b343e459c07
Sören Kuklau <chucker23n+github@gmail.com>
2023-07-11 11:14:07 +0200

    #36: tests! 🧪

---
+++ TRENZ.Docs.API.Test/PathTraversalTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Security;
+
+namespace TRENZ.Docs.API.Test;
+
+[TestClass]
+[TestCategory("Services")]
+public class PathTraversalTests
+{
+    private const string MacOsRoot = "/var/folders/ab/cdefg/T/TRENZ.Docs.API/trenz-docs-test";
+
+    [DataTestMethod]
+    [DataRow(MacOsRoot, "public/", $"{MacOsRoot}/public/")]
+    [DataRow(MacOsRoot, "./", $"{MacOsRoot}/./")]
+    public void TestAllowsPaths(string root, string path, string expectedFullRoot)
+    {
+        var pathTraversalService = TestHelper.GetPathTraversalService();
+
+        Assert.AreEqual(expectedFullRoot, pathTraversalService.Traverse(root, path));
+    }
+
+    [DataTestMethod]
+    [DataRow(MacOsRoot, "../")]
+    [DataRow(MacOsRoot, "/")]
+    public void TestThrowsForPaths(string root, string path)
+    {
+        var pathTraversalService = TestHelper.GetPathTraversalService();
+
+        Assert.ThrowsException<SecurityException>(() =>
+        {
+            pathTraversalService.Traverse(root, path);
+        });
+    }
+}
