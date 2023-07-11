78dd8092f5e2e79fbf013d3e06581b032fe661da
Sören Kuklau <chucker23n+github@gmail.com>
2023-07-11 11:52:24 +0200

    #36: more tests

--- TRENZ.Docs.API.Test/PathTraversalTests.cs
+++ TRENZ.Docs.API.Test/PathTraversalTests.cs
@@ -7,21 +7,46 @@ namespace TRENZ.Docs.API.Test;
 public class PathTraversalTests
 {
     private const string MacOsRoot = "/var/folders/ab/cdefg/T/TRENZ.Docs.API/trenz-docs-test";
+    private const string DotRoot = "./Data";
+    private const string WindowsRoot = @"D:\www\docs.my.example";
 
     [DataTestMethod]
     [DataRow(MacOsRoot, "public/", $"{MacOsRoot}/public/")]
     [DataRow(MacOsRoot, "./", $"{MacOsRoot}/./")]
-    public void TestAllowsPaths(string root, string path, string expectedFullRoot)
+    [DataRow(WindowsRoot, @"public\", @$"{WindowsRoot}\public\")]
+    public void TestResolvesPathAs(string root, string path, string expectedFullRoot)
     {
         var pathTraversalService = TestHelper.GetPathTraversalService();
 
-        Assert.AreEqual(expectedFullRoot, pathTraversalService.Traverse(root, path));
+        expectedFullRoot = NormalizeDirectorySeparatorChar(expectedFullRoot);
+
+        var traversedPath = pathTraversalService.Traverse(root, path);
+
+        traversedPath = NormalizeDirectorySeparatorChar(traversedPath);
+
+        Assert.AreEqual(expectedFullRoot, traversedPath);
+
+        static string NormalizeDirectorySeparatorChar(string expectedFullRoot)
+        {
+            foreach (var item in new[] { @"\", "/" })
+                expectedFullRoot = expectedFullRoot.Replace(item, Path.DirectorySeparatorChar.ToString());
+            return expectedFullRoot;
+        }
+    }
+
+    [DataTestMethod]
+    [DataRow(DotRoot, "./")]
+    public void TestDoesNotThrowForPath(string root, string path)
+    {
+        var pathTraversalService = TestHelper.GetPathTraversalService();
+
+        Assert.IsNotNull(pathTraversalService.Traverse(root, path));
     }
 
     [DataTestMethod]
     [DataRow(MacOsRoot, "../")]
     [DataRow(MacOsRoot, "/")]
-    public void TestThrowsForPaths(string root, string path)
+    public void TestThrowsForPath(string root, string path)
     {
         var pathTraversalService = TestHelper.GetPathTraversalService();
 
